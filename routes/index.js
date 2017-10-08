d28aa8224cec7771bf0f04c682d8a1c7bf485ce4
Lakshmi <lakshmi.pillai@infosys.com>
2017-10-08 13:15:47 +0530

    API and testing setup.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'MusicList Alpha' });
+  res.render('index', {
+    title: 'MusicList Alpha',
+  });
 });
 
-
-  router.get('/sendjson', (req, res, next) => {
-  	res.json(testJSON);
-  })
 
 module.exports = router;
